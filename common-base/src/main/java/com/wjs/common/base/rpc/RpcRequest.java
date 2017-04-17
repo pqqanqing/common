9dc86b0c929f5a7f5dada0cdfccf2d2e3a611992
pqqanqing <15601870402@163.com>
2017-04-17 10:04:44 +0800

    添加基础模块文件

--- common-base/src/main/java/com/wjs/common/base/rpc/RpcRequest.java
+++ common-base/src/main/java/com/wjs/common/base/rpc/RpcRequest.java
@@ -14,12 +14,30 @@ import java.util.Date;
 @Setter
 @Getter
 public class RpcRequest extends BaseObject {
-    private String appId;//所属应用编号
-    private String requestId;//请求编号
-    private Date requestTime;//请求时间
-    private String version;//使用的交互协议版本号
-    private String authType;//认证方式
-    private String authInfo;//认证信息
+    /**
+     * 所属应用编号
+     */
+    private String appId;
+    /**
+     * 请求编号
+     */
+    private String requestId;
+    /**
+     * 请求时间
+     */
+    private Date requestTime;
+    /**
+     * 使用的交互协议版本号
+     */
+    private String version;
+    /**
+     * 认证方式
+     */
+    private String authType;
+    /**
+     * 认证信息
+     */
+    private String authInfo;
 
     public RpcRequest() {
     }
